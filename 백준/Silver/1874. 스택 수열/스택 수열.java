39fa0a1a69bccf65b4f34b0180fac846231a7f49
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-04-11 16:09:07 +0900

    [Silver II] Title: 스택 수열, Time: 1024 ms, Memory: 111196 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,43 +2,39 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-   
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); //수열의 크기를 사용자로부터 입력 받음
-        int [] A = new int[N]; //크기 N인 배열 A 선언
-       
-        for(int i=0; i < N; i++) {
-                                        A[i]  = sc.nextInt();    
-                                     }
-            //배열에 사용자 입력을 받아 N개의 수열 값 저장
-       
-        Stack<Integer> stack = new Stack<>(); // 스택 생성
-        StringBuffer bf = new StringBuffer(); // +,- 저장되는 버퍼생성
-        int num = 1; // 스택에 push or pop 되는 오름차순 숫자
-        boolean result = true;
-       
-        for(int i =0; i < A.length; i++) {
-            int su = A[i];
-            if(su >= num) {
-                 while(su >= num) {
-                     stack.push(num++);
-                     bf.append("+\n");
-                 }
-                 stack.pop();
-                 bf.append("-\n");
-            }
-            else {
-                int p = stack.pop();
-                if(p != su) {
-                     System.out.println("NO");
-                     result = false; // 출력제어, bf에 저장된 값 출력 금지
-                     break;
-                }//수열수와 일치하지 않아 수열생성 불가
-                else {  bf.append("-\n");   }
-           }    
-        } 
-        if(result)System.out.println(bf.toString());
-        sc.close();
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Stack<Integer> stack = new Stack<>(); // 스택 선언
+		StringBuffer bf = new StringBuffer(); // +,- 저장할 버퍼
+		
+		int N = sc.nextInt(); // 수열의 갯수
+		int[] arr = new int[N]; // 수열을 받는 배열
+		int num = 1; // 스택에 들어갈 수
+		boolean yes = true; // 수열을 만들 수 있는지 체크할 변수
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt(); // 값 받기
+		}
+		
+		for(int i=0; i<N; i++) {
+			if(num <= arr[i]) { // 만약 넣을 값이 수열수보다 작다면 
+				while(num <= arr[i]) { 
+                    stack.push(num++);
+					bf.append("+\n");
+				}
+				stack.pop();
+			  bf.append("-\n"); // 수열수과 스택에 들어간 값이 같은 상태
+			}
+			else { // 넣을 값이 수열보다 크다면
+				if(stack.pop() == arr[i]) {									 // 때문에 pop과 수열 수가 같다면 다음 수열로, 다르다면 바로 될 수 없음을 알리고 break로 모든 과정을 빠져나옴
+				  bf.append("-\n");
+				}
+				else {
+					yes = false; // 수열 생성 안됨 체크
+					break; // for문(수열 순회) 종료
+				}
+			}
+		}
+		System.out.println(yes ? bf.toString() : "NO"); // break 됐다면 NO가 나올 것이고, 진행이 잘 됐다면 +- 결과가 나올 것임
+	}
 }
